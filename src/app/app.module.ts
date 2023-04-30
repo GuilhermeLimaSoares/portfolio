763a594f3bec9cc62a7d10167e9dcec10dd0a666
GuilhermeLimaSoares <guilhermelimasoares9@gmail.com>
2023-04-30 15:04:14 -0300

    Create main section with basic informations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import {MatCommonModule} from '@angular/material/core';
+import {MatIconModule} from '@angular/material/icon';
+
 @NgModule({
   declarations: [
     AppComponent
@@ -12,7 +15,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatCommonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
